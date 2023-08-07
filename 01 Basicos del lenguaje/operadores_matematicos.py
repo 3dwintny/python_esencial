a864d728316597ef9a1cfcf9c2cd82cdf8290599
3dwintny <tecnotony8@gmail.com>
2023-08-07 14:47:01 -0600

    Operadores matematicos

---
+++ 01 Basicos del lenguaje/operadores_matematicos.py
@@ -0,0 +1,27 @@
+# estos son los operadores que podemos tener en python
+# +, -, /, //, %, *, **
+#Podemos ver el tipo de datos
+tipo1 = {type("Hola como estas")},{type(5)},{type(5.5)},{type(True)}, {type(3+ 5j)}, {type([3,2,1])} 
+
+for tipo in tipo1:
+    print("Con type, sabemos el tipo de dato, por ejemplo",tipo)
+    
+num1 = 2
+num2 = 3
+
+res = num1*num2
+print("El resultado es:",res)
+    
+#Variable
+# Variables de una asignacion
+message = "como estas?"
+_age = 24
+PI  = 3.12159
+__do_not_touch = "Esta variable es importante para el programa, porfavor no modificarla"
+#variables que se pueden reasignacion
+
+var_1= 5
+var_1= var_1+5
+print(var_1)
+ 
+
