31dd7c4de667ade0b51a3aaa97b276242e53676a
3dwintny <tecnotony8@gmail.com>
2023-08-07 12:42:29 -0600

    Agregando primeros cambios

---
+++ 01 Basicos del lenguaje/Que_es_la_programacion.py
@@ -0,0 +1,39 @@
+
+#Introduccion al lenguaje de python
+
+#Que es la programacion?
+#una serie de instrucciones que descibe como realizar un computo(instrucciones que le damos).
+
+#Objetivos del curos:
+# Aprender a pensar como un Cienfifico del comput
+# aprender a utilizar Python
+# Entender las ventajas y desventajas de python
+# Aprender a contruir una aplicacion de liea de comandos.
+
+# Razones por las que usar python
+    # Uno de los mejor lenguajes para los principiantes
+    # Comunidad
+    # Facilidad de uso, no es el mas rapido pero en ocaciones no es tan importante que sea el mas rapido.
+    # Librerias
+    # Popularidad, Es uno de los top 5 en 2018
+    # industria, Varios gigantes de la industria lo usan.
+
+#
+import turtle
+
+window = turtle.Screen()
+tortuga = turtle.Turtle()
+
+tortuga.forward(100)
+tortuga.right(90)
+
+tortuga.forward(90)
+tortuga.right(90)
+
+tortuga.forward(100)
+tortuga.right(90)
+
+tortuga.forward(80)
+tortuga.right(90)
+
+window.mainloop()
---
+++ 01 Basicos del lenguaje/ejemplos_turtle.py
@@ -0,0 +1,41 @@
+import turtle
+
+window = turtle.Screen()
+tortuga = turtle.Turtle()
+
+tortuga.fd(100)
+tortuga.right(90)
+
+tortuga.fd(40)
+tortuga.right(90)
+
+tortuga.fd(70)
+tortuga.left(90)
+
+tortuga.fd(20)
+tortuga.left(90)
+
+tortuga.fd(70)
+tortuga.right(90)
+
+tortuga.fd(40)
+tortuga.right(90)
+
+tortuga.fd(70)
+tortuga.left(90)
+
+tortuga.fd(20)
+tortuga.left(90)
+
+tortuga.fd(70)
+tortuga.right(90)
+
+tortuga.fd(40)
+tortuga.right(90)
+
+tortuga.fd(100)
+tortuga.right(90)
+
+tortuga.fd(160)
+
+window.mainloop()
